ec6a01024aff8cdeb4174315bff9ce1ea1983a7d
Saurabh Ranjan <saurabh.ranjan@hestabit.in>
2021-09-12 17:43:26 +0530

    on double click reminder class added

--- src/App.js
+++ src/App.js
@@ -16,16 +16,38 @@ import {useState} from 'react';
           text: 'Saurabh Anniversary',
           day: 'Sep 4th, 2021',
           reminder: false
-      }])
+      },
+      {
+        id: 3,
+        text: 'Learning React',
+        day: 'Sep 12th, 2021',
+        reminder: true
+      },
+      {
+        id: 4,
+        text: 'Updating git',
+        day: 'Sep 14th, 2021',
+        reminder: true
+    }
+    ])
 
+    // delete task by id
     const deleteTask = (taskId) => {
-      console.log('Delete Task Action Called', taskId)
+      setTasks(tasks.filter((task) => task.id !== taskId))
+      console.log(`Task with ${taskId} is deleted.`);
     };
 
+    // toggle reminder on double click
+    const reminderToggle = (taskId) => {
+      console.log('Task reminder db clicked on ', taskId);
+      setTasks(tasks.map((task) => task.id === taskId ? {
+        ...task, reminder: !task.reminder
+      }: task))
+    }
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      { tasks.length > 0 ? <Tasks tasks={tasks} onToggle={reminderToggle} onDelete={deleteTask}/> : 'No tasks' }
     </div>
   );
 }
